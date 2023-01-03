4283e4f83170bb08f621b2bf8a3adbaac0ca4e33
OmarPorf <omaritoxdpg@gmail.com>
2023-01-02 21:15:52 -0600

    Programacion funcional: generadores y arboles

---
+++ labp8/arbolito.py
@@ -0,0 +1,33 @@
+#===================================================
+#   Graficos usando la tortuga que pinta al caminar
+#===================================================
+
+import turtle
+tortuga = turtle.Turtle()
+tortuga.left(90)    #   Giro a la izquierda de 90 grados
+tortuga.speed(500)  #   Velocidad de la tortuga
+
+#===================================
+#   Con angulos de 90 es un arbol H
+#===================================
+angulo: float = 90
+
+#==========================================
+#   El arbol se construye con recursividad
+#==========================================
+
+def arbol(i: float, angulo: float):
+    if i < 10.0:
+        return
+    else:
+        tortuga.forward(i)  #   Camina i unidades
+        tortuga.left(angulo)    #   Gira a la izquierda 90 grados
+        arbol(3.0 * i / 4.25, angulo)   #   Pide otro arbol y cambia
+                                        #   la longitud del paso
+        tortuga.right(2 * angulo)   #   Gira a la derecha 180
+        arbol(3.0 * i / 4.25, angulo)
+        tortuga.left(angulo)
+        tortuga.backward(i)
+
+arbol(100, angulo)
+turtle.done()
